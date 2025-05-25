cb294e04fb97e161412c54a29069feb2f0c92653
Flo-Buc <flobu320@gmail.com>
2025-05-25 11:39:59 +0200

    Update step_4.R

--- assignment_1/step_4.R
+++ assignment_1/step_4.R
@@ -3,6 +3,8 @@ orb_berlin <- readRDS("~/github/rct25/data/generated/Orbis_Berlin_Data/orbis_pan
 
 library(dplyr)
 
+
+
 # Filter the data for the specific postal code and year
 filtered_data_step4 <- orb_berlin %>%
   filter(postcode == "10437" & year == 2021)
@@ -11,11 +13,14 @@ filtered_data_step4 <- orb_berlin %>%
 if (nrow(filtered_data_step4) > 0) {
   company_with_highest_assets <- filtered_data_step4 %>%
     arrange(desc(toas)) %>%
-    slice(1) %>% 
-    select(name_native, toas)
+    slice(1) %>%
+    select(name_native, toas, shfd, turn)
   
-  cat("Company with the highest financial assets in postcode 10437 for the year 2021:\n")
-  print(company_with_highest_assets)
+  cat("The company with the highest financial assets in postcode 10437 for the year 2021 is:\n")
+  cat("Company Name: ", company_with_highest_assets$name_native, "\n")
+  cat("Total Assets (toas): ", company_with_highest_assets$toas, "\n")
+  cat("Book Value of Equity (shfd): ", company_with_highest_assets$shfd, "\n")
+  cat("Net Sales (turn): ", company_with_highest_assets$turn, "\n")
 } else {
   cat("No data found for postcode 10437 in the year 2021.\n")
 }
